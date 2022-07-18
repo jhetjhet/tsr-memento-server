aecfd2436409cde97bf80d857ea840316b263824
austin <villasanamallariaustin@gmail.com>
2022-07-18 12:18:24 +0800

    nodejs-sync validation
    call field validate synchronously and make record validate function async

--- nodejs-backend/models/record/index.js
+++ nodejs-backend/models/record/index.js
@@ -21,19 +21,18 @@ const recordSchema = new Schema({
         type: Map,
         of: Schema.Types.Mixed,
         default: {},
-        validate: function(map){
-
+        validate: async function(map){
             const validated_map = new Map();
             for(var val of map.keys()){
                 let fdata = map.get(val);
-                fields.FieldSchema.validate(fdata);
+                await fields.FieldSchema.validate(fdata);
 
                 let model_class = fields[fdata.field_type];
-                model_class.validate(fdata);
+                await model_class.validate(fdata);
 
                 validated_map.set(val, new model_class(fdata));
             }
-
+            console.log("PROCEEDED !!")
             this.__fields__ = validated_map;
             return true;
         },
