a087383622f3d74e5e634200e9b3a5d1686e9bfe
austin <villasanamallariaustin@gmail.com>
2022-07-25 00:19:00 +0800

    nodejs-separate record format and records
    recordFormat is for schemas storage and records is for collections of data with the schema define in recordFormat

---
+++ nodejs-backend/middlewares/recordFormatMiddlewares.js
@@ -0,0 +1,75 @@
+const RecordFormatSchema = require('../models/record/recordFormatSchema');
+
+const recordDocument = async (req, res, next) => {
+    const { record_id } = req.params;
+    try {
+        const record = await RecordFormatSchema.findById(record_id);
+        if(record){
+            req.record_doc = record;
+            return next();
+        }
+
+        return res.status(404).end();
+    } catch (error) {
+        return next(error);
+    }
+}
+
+const create = async (req, res, next) => {
+    try {
+        const record = RecordFormatSchema(req.body);
+        await record.save();
+        return res.json(record);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+const lists = async (req, res, next) => {
+    try {
+        const records = await RecordFormatSchema.find();
+        return res.json(records);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+const retrieve = [
+    recordDocument,
+    (req, res) => {
+        res.json(req.record_doc);
+    },
+];
+
+const update = [
+    recordDocument,
+    async (req, res, next) => {
+        const record = req.record_doc;
+        try {
+            Object.assign(record, req.body);
+            await RecordFormatSchema.save();
+            return res.json(record);
+        } catch (error) {
+            return next(error);
+        }
+    }
+]
+
+const _delete = async (req, res, next) => {
+    const { record_id } = req.params;
+    try {
+        await RecordFormatSchema.deleteOne({_id: record_id});
+        return res.status(201).end();
+    } catch (error) {
+        return next(error);
+    }
+}
+
+module.exports = {
+    recordDocument,
+    create,
+    lists,
+    retrieve,
+    update,
+    delete: _delete,
+};
