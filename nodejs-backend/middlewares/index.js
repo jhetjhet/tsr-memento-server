cbd1741261903f62186b1376cad239f7c92cceba
austin <villasanamallariaustin@gmail.com>
2022-08-19 13:24:23 +0800

    nodejs-middlewares index refactor errorHandler
    return 401 for any errors that is instance of jwt errors

--- nodejs-backend/middlewares/index.js
+++ nodejs-backend/middlewares/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { validationResult } = require('express-validator');
-const { TokenExpiredError } = require('jsonwebtoken');
+const { TokenExpiredError, JsonWebTokenError, NotBeforeError } = require('jsonwebtoken');
 
 const validationError = (req, res, next) => {
     const errors = validationResult(req);
@@ -20,9 +20,9 @@ const errorHandler = (err, req, res, next) => {
     if(err instanceof mongoose.Error.ValidationError)
         return res.status(400).json(err.errors);
 
-    if(err instanceof TokenExpiredError)
-        return res.status(401).end("Toke expired.");
-        
+    if(err instanceof TokenExpiredError || err instanceof JsonWebTokenError || err instanceof NotBeforeError)
+        return res.status(401).end();
+
     next();
 }
 
