6e5ffd1f0360d5b2b3d311b096701c426f52b0ac
austin <villasanamallariaustin@gmail.com>
2022-08-20 13:00:44 +0800

    nodejs-add background prop on user

--- nodejs-backend/models/user/index.js
+++ nodejs-backend/models/user/index.js
@@ -14,7 +14,8 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true,
-    }
+    },
+    background: String,
 });
 
 userSchema.pre('save', async function(next) {
